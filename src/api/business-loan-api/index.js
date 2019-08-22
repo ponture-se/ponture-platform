174cb07176ac28ac78661ce5c1e03558bb1ed144
saeedPadyab <padyabsaeed@gmail.com>
2019-08-22 10:51:54 +0300

    remove console from code

--- src/api/business-loan-api/index.js
+++ src/api/business-loan-api/index.js
@@ -645,7 +645,6 @@ export function submitLoan() {
   const _call = loan => {
     const url = submitUrl;
     const token = Cookies.get("@pontrue-wizard/token");
-    console.log(loan);
     axios({
       method: "post",
       url: url,
