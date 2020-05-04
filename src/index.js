e4a4bb75ee964915bfdee80fc9e77c564212b1f8
saeedPadyab <padyabsaeed@gmail.com>
2020-05-04 12:46:39 +0300

    add cssVar ponyfill

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/ie9";
 import "react-app-polyfill/stable";
+import cssVars from "css-vars-ponyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import initScripts from "./initScripts";
 import * as serviceWorker from "./serviceWorker";
 initScripts();
+cssVars();
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
