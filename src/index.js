373e5bade219691a0bc2426988f61bb1029b0fb7
saeedPadyab <padyabsaeed@gmail.com>
2020-05-04 14:23:58 +0300

    removed script configs

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-initScripts();
 cssVars();
 ReactDOM.render(<App />, document.getElementById("root"));
 
