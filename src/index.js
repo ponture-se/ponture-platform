23422682dcd32bf1620f2248df89b040731520da
saeedPadyab <padyabsaeed@gmail.com>
2020-05-14 13:18:11 +0300

    Revert "Merge branch 'B-fix-caching' into development-cpanel"
    This reverts commit 4e763c510e7145eb4123a41296f9d47ae7fe7902, reversing
    changes made to ffaa740580c7725f7192c90404af81a72d2a1cf2.

--- src/index.js
+++ src/index.js
@@ -5,26 +5,14 @@ import cssVars from "css-vars-ponyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import CacheBuster from "./CacheBuster";
 import * as serviceWorker from "./serviceWorker";
 
 cssVars({
   preserveStatic: false,
 });
-ReactDOM.render(
-  <CacheBuster>
-    {({ loading, isLatestVersion, refreshCacheAndReload }) => {
-      if (loading) return null;
-      if (!loading && !isLatestVersion) {
-        refreshCacheAndReload();
-      }
-      return <App />;
-    }}
-  </CacheBuster>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
