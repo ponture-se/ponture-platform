455eae708078a2652a513e3a965305668a8d24ab
saeedPadyab <padyabsaeed@gmail.com>
2020-05-29 12:45:31 +0300

    goup component

--- src/Pages/BusinessLoan/components/Content.jsx
+++ src/Pages/BusinessLoan/components/Content.jsx
@@ -3,25 +3,40 @@ import styles from "../styles.module.scss";
 import Profile from "./Profile";
 import Form from "./Form";
 import NoBankIdAlert from "./NoBankIdAlert";
+import GoUp from "./GoUp";
 import { useLoanState } from "hooks/useLoan";
 
 const Content = (props) => {
-  const { formStatus } = useLoanState();
+  const { formStatus, steps } = useLoanState();
+  const [goUpState, toggleGoUp] = React.useState(false);
+  React.useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset < 1000) toggleGoUp(false);
+      else toggleGoUp(true);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
   return (
-    <div className={styles.content}>
-      {formStatus === "form" ? (
-        <>
-          <div className={styles.profile}>
-            <Profile />
-          </div>
-          <div className={styles.form}>
-            <Form />
-          </div>
-        </>
-      ) : formStatus === "noNeedBankId" ? (
-        <NoBankIdAlert />
-      ) : null}
-    </div>
+    <>
+      <div className={styles.content}>
+        {formStatus === "form" ? (
+          <>
+            <div className={styles.profile}>
+              <Profile />
+            </div>
+            <div className={styles.form}>
+              <Form />
+            </div>
+          </>
+        ) : formStatus === "noNeedBankId" ? (
+          <NoBankIdAlert />
+        ) : null}
+      </div>
+      {goUpState && <GoUp />}
+    </>
   );
 };
 
